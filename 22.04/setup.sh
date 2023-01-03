5625125c626b43034f95af030d2b4c6307c624e5
goodahn <wnaldj1589@gmail.com>
2023-01-04 03:17:12 +0900

    소프트웨어 설치가 정상적으로 이뤄졌는지 확인하는 부분 추가

--- 22.04/setup.sh
+++ 22.04/setup.sh
@@ -1,8 +1,50 @@
+setup_color() {
+  # Only use colors if connected to a terminal
+  if ! is_tty; then
+    FMT_RAINBOW=""
+    FMT_RED=""
+    FMT_GREEN=""
+    FMT_YELLOW=""
+    FMT_BLUE=""
+    FMT_BOLD=""
+    FMT_RESET=""
+    return
+  fi
+
+  if supports_truecolor; then
+    FMT_RAINBOW="
+      $(printf '\033[38;2;255;0;0m')
+      $(printf '\033[38;2;255;97;0m')
+      $(printf '\033[38;2;247;255;0m')
+      $(printf '\033[38;2;0;255;30m')
+      $(printf '\033[38;2;77;0;255m')
+      $(printf '\033[38;2;168;0;255m')
+      $(printf '\033[38;2;245;0;172m')
+    "
+  else
+    FMT_RAINBOW="
+      $(printf '\033[38;5;196m')
+      $(printf '\033[38;5;202m')
+      $(printf '\033[38;5;226m')
+      $(printf '\033[38;5;082m')
+      $(printf '\033[38;5;021m')
+      $(printf '\033[38;5;093m')
+      $(printf '\033[38;5;163m')
+    "
+  fi
+
+  FMT_RED=$(printf '\033[31m')
+  FMT_GREEN=$(printf '\033[32m')
+  FMT_YELLOW=$(printf '\033[33m')
+  FMT_BLUE=$(printf '\033[34m')
+  FMT_BOLD=$(printf '\033[1m')
+  FMT_RESET=$(printf '\033[0m')
+}
+
+# 1. git, 2. vim, 3. tmux, 4. zsh 설치
 sudo add-apt-repository -y ppa:jonathonf/vim
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4AB0F789CBA31744CC7DA76A8CF63AD3F06FC659
 sudo apt update -y
-
-# 1. git, 2. vim, 3. tmux, 4. zsh 설치
 sudo apt install -y git vim tmux zsh
 
 # 4. oh-my-zsh 설치
@@ -47,3 +89,62 @@ echo 'export PATH="$PATH:$GOPATH/bin"' >> ~/.zshrc
 
 # 8. nvm 설치
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
+# 9. 소프트웨어 설치 확인
+setup_color
+
+# 9.1 git 설치 확인
+if git --version > /dev/null; then
+    printf '%s git installation succeed \n' $FMT_GREEN
+else
+    printf '%s git installation failed \n' $FMT_RED
+fi
+
+# 9.2 vim 설치 확인
+if vim --version > /dev/null; then
+    printf '%s vim installation succeed \n' $FMT_GREEN
+else
+    printf '%s vim installation failed \n' $FMT_RED
+fi
+
+# 9.3 tmux 설치 확인
+if tmux -V > /dev/null; then
+    printf '%s tmux installation succeed \n' $FMT_GREEN
+else
+    printf '%s tmux installation failed \n' $FMT_RED
+fi
+
+# 9.4 zsh 설치 확인
+if which zsh > /dev/null; then
+    printf '%s vim installation succeed \n' $FMT_GREEN
+else
+    printf '%s vim installation failed \n' $FMT_RED
+fi
+
+# 9.5 docker 설치 확인
+if docker --version > /dev/null; then
+    printf '%s docker installation succeed \n' $FMT_GREEN
+else
+    printf '%s docker installation failed \n' $FMT_RED
+fi
+
+# 9.6 pyenv 설치 확인
+if pyenv --version > /dev/null; then
+    printf '%s pyenv installation succeed \n' $FMT_GREEN
+else
+    printf '%s pyenv installation failed \n' $FMT_RED
+fi
+
+# 9.7 goenv 설치 확인
+if goenv --version > /dev/null; then
+    printf '%s goenv installation succeed \n' $FMT_GREEN
+else
+    printf '%s goenv installation failed \n' $FMT_RED
+fi
+
+# 9.8 nvm 설치 확인
+if nvm --version > /dev/null; then
+    printf '%s nvm installation succeed \n' $FMT_GREEN
+else
+    printf '%s nvm installation failed \n' $FMT_RED
+fi
